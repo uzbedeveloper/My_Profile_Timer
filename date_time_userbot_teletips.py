3b8e79a544072469e0fa0dc3fa0a437af4fd6c82
uzbedeveloper <103754533+uzbedeveloper@users.noreply.github.com>
2022-08-06 12:23:29 +0500

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -1,29 +1,4 @@
-#Copyright ©️ 2021 TeLe TiPs. All Rights Reserved
-#You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
-# ##Credits - [DATE_TIME Telegram userbot by TeLe TiPs] (https://github.com/teletips/DATE_TIME_USERBOT-TeLeTiPs)
-
-# Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/teletips/DATE_TIME_USERBOT-TeLeTiPs/blob/main/LICENSE
-
-from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
-from lists_teletips.quotes_teletips import *
-from lists_teletips.emojis_teletips import *
-from PIL import Image, ImageDraw, ImageFont
-import datetime
-import pytz
-import asyncio
-import random
-import os
- 
-
-async def main_teletips():
-    try:
-        while True:
-            print("Hi") 
-                
-            await asyncio.sleep(10) 
-    except FloodWait as e:
-        await asyncio.sleep(10)         
-
-print("DATE TIME USERBOT IS ALIVE!")
-asyncio.ensure_future(main_teletips())
+x: int = 0
+while True:
+    x= x+1
+    print(x)
